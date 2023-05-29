71e270f00f5b93bcfda7e8d0381bdc2e08bd9a6d
VictoriaRoshka <kawaii0109@mail.ru>
2023-05-29 19:40:22 +0300

    руализована функция добавления элемента

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -3,6 +3,7 @@ import Button from 'components/button';
 import Header from 'components/header';
 import { useState } from 'react';
 import Element from 'components/element';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
@@ -26,6 +27,17 @@ function App() {
     setGoods(filteredElement)
   }
 
+  const [newElement, setNewElement] = useState('')
+
+  const addElement = () => {
+    const newProduct = {
+      id: uuidv4,
+      name: newElement
+    }
+    setGoods([...goods, newProduct])
+  }
+
+
   return (
     <div>
       <Header/>
@@ -33,8 +45,8 @@ function App() {
       <main className='bg-gray-100 max-w-screen-lg mx-auto'>
         <div className='max-w-5xl mx-auto py-10 font-serif'>
           <div className='flex gap-4 mx-5 justify-center inputBox'>
-          <input className='border-4'/>
-          <Button title="Добавить"/>
+          <input className='border-4' value={newElement} onChange={(event) => setNewElement(event.target.value)}/>
+          <Button title="Добавить" onClick={addElement}/>
           </div>
           <div>
             {goods.length === 0 && (<div className='text-center text-blue-300 text-2xl px-5 py-5'>Список пуст</div>)}
--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,7 +1,7 @@
 function Button(props){
-    const {title} = props
+    const {title, onClick} = props
     return(
-        <button className="bg-blue-500 mx-10 rounded-lg text-xl text-white px-4 py-2">
+        <button className="bg-blue-500 mx-10 rounded-lg text-xl text-white px-4 py-2" onClick={onClick}>
             {title}
         </button>
     )
